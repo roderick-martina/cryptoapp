014bab966c88068e223154a80cf50ebb775432b8
Roderick Martina <roderick.martina.p@gmail.com>
2018-04-12 19:38:48 +0200

    fixed first enter reload

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container mx-auto">
-    <form class="mx-auto bg-white rounded shadow-md outline-none mt-4 flex items-center" style="height:4.5rem;">
+    <form class="mx-auto bg-white rounded shadow-md outline-none mt-4 flex items-center" style="height:4.5rem;" @click.prevent>
         <input  @keyup.enter="search()" v-model="searchQuery"  ref="searchBox" class="w-full py-4 px-6 font-normal text-xl outline-none" type="search" placeholder="Zoek Crypto Valuta...."/>
         <button ref="searchButton" class="bg-brand-blue outline-none py-3 px-4 rounded text-white text-lg mr-4">
             <svg class="h-6 w-6 fill-current text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/></svg>
@@ -59,14 +59,13 @@ export default {
         this.showDropdown = true
       }
     },
+    //click event for search button @click.native does not work at the moment
     searchButton(e) {
       let el = this.$refs.searchButton
       let target = e.target;
       if(el == target && el.contains(target)) {
         this.search();
       } 
-      
-
     },
     test(){
       console.log('sadasda')
