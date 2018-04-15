9f25476c0e15c9ae07285dcfb7628de750846fe5
Roderick Martina <roderick.martina.p@gmail.com>
2018-04-15 15:20:23 +0200

    modified crypotiles component

--- src/components/CryptoTitles.vue
+++ src/components/CryptoTitles.vue
@@ -1,11 +1,11 @@
 <template>
-   <div class="md:flex md:flex-row hidden md:mt-10 mt-4 text-brand-grey-dark">
+   <div class="md:flex md:flex-row md:mt-10 mt-4 hidden text-brand-grey-dark">
                   <span class="mx-6">#</span>
                   <span class="w-64">Name And Description</span>
-                  <span class="flex-1">Marketcap</span>
+                  <span class="md:flex-1 md:block hidden">Marketcap</span>
                   <span class="flex-1">Price</span>
-                  <span class="flex-1">Volume (24)</span>
-                  <span class="flex-1">Circulating Supply</span>
+                  <span class="md:flex-1 md:block hidden">Volume (24)</span>
+                  <span class="md:flex-1 md:block hidden">Circulating Supply</span>
                   <span class="flex-1 text-center">Change (24h)</span>
                   <!-- <span class="flex-1">Price graph (7d)</span> -->
               </div>
