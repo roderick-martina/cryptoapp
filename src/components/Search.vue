e9e04b32e69488c12f8309d3cf86190ad8c9fb63
roderick martina <roderick.martina.p@gmail.com>
2019-02-11 13:57:57 +0100

    fixed search button bug

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="container mx-auto w-full">
     <form ref="dropdownSearchBox" class=" md:mx-auto mx-4 bg-white rounded shadow-md outline-none mt-4 flex items-center" style="height:4.5rem;" @click.prevent>
-        <input  @keyup.enter="search()" v-model="searchQuery"   class="w-full py-4 px-6 font-normal text-xl outline-none" type="search" placeholder="Search Crypto Currency..."/>
-        <button @click="search()" ref="searchButton" class="bg-brand-blue outline-none py-3 px-4 rounded text-white text-md absolute pin-r md:mr-32 mr-8">
+        <input  @keyup.enter="search()" v-model="searchQuery"   class="md:w-full py-4 px-6 font-normal text-xl outline-none" type="search" placeholder="Search Crypto Currency..."/>
+        <button @click="search()" ref="searchButton" class="bg-brand-blue outline-none py-3 px-4 rounded text-white mr-8">
             <svg class="h-6 w-6 fill-current text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/></svg>
         </button>
         <div class="md:absolute container">
